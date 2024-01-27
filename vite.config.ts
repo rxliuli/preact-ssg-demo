66a7907fb59d9bb1ce8951b986da1c9351645e68
rxliuli <rxliuli@gmail.com>
2024-01-27 16:27:32 +0900

    chore: add test mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-			},
-		}),
-	],
-});
+export default defineConfig(({ mode }) => {
+  if (mode === 'test') {
+    return {
+      plugins: [
+        {
+          name: 'test',
+          options(opts) {
+            opts.input = ['./src/index.tsx']
+            opts.preserveEntrySignatures = 'allow-extension'
+          },
+        },
+      ],
+      build: {
+        minify: false,
+      },
+    }
+  }
+  return {
+    plugins: [
+      preact({
+        prerender: {
+          enabled: true,
+          renderTarget: '#app',
+        },
+      }) as any,
+    ],
+  }
+})
